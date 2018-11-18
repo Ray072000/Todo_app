b5f333485710fd155399b63c6d4864a78d470fd5
Raymond Sanchez <coolraymond123@gmail.com>
2018-11-18 19:24:52 +0000

    Add the RCAV flows for the new and create actions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,33 +2,16 @@ class TodoController < ApplicationController
      def index
      end
      def show
-          @todo_description = "something new"
-          @todo_pomodoro_estimate = 4
-          todo_id= params[:id]
-          if todo_id=="1"
-               @todo_description= "make a curriculum"
-                @todo_pomodoro_estimate = 4
-          elsif todo_id=="2"
-                @todo_description= "Ap chem"
-                @todo_pomodoro_estimate = 5
-           elsif todo_id=="3"
-                @todo_description= "finish my english homework"
-                @todo_pomodoro_estimate = 4
-          elsif todo_id=="4"
-                @todo_description= "do spanish homework"
-                @todo_pomodoro_estimate = 3
-          elsif todo_id=="5"
-                @todo_description= "workout"
-                @todo_pomodoro_estimate = 2
-          elsif todo_id=="6"
-                @todo_description= "watch tech videos"
-                @todo_pomodoro_estimate = 1
-          elsif todo_id=="7"
-                @todo_description= "sleep"
-                @todo_pomodoro_estimate = 0
-           elsif todo_id=="8"
-                @todo_description= "finish"
-                @todo_pomodoro_estimate = 10
+        @todo = Todo.find_by_id(params[:id])
+    end
+          def new
           end
-     end
+        def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+   
+        end
 end
